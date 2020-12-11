4863fa4b3dbb935d634333f310521a3cb516eaf2
Bryan Evangelista <bryanevangelista@Bryans-MBP.socal.rr.com>
2020-12-11 10:30:07 -0800

    fixed towersim

--- Homework 3/simulation/TowerSimulator.py
+++ Homework 3/simulation/TowerSimulator.py
@@ -3,19 +3,21 @@ from Plane import Plane
 
 
 class TowerSimulator:
-   def __init__(self, length, odds_Plane_arrives):
+   def __init__(self, length, odds_to_land_arrival, odds_to_takeoff_arrival):
       self._length = length
-      self._odds_Plane_arrives = odds_Plane_arrives
+      self._odds_to_land_arrival = odds_Plane_arrives
+      self._odds_to_takeoff_arrival = odds_to_takeoff_arrival
       self._Tower = Tower()
-      self._Tower2 = Tower()
 
    def run_simulation(self):
       """ The following loop controls our entire simulation
       Each iteration represents a single clock tick (i.e. a minute) """
       for i in range(self._length):
+         random_gen = random.random()
+
 
          # See if a new Plane is created this clock tick
-         new_Plane = Plane.generate_Plane(self._odds_Plane_arrives, i)
+         new_Plane = Plane.generate_Plane(self._odds_to_land_arrival, self._odds_to_takeoff_arrival, i)
 
          # If it is, add it to the Tower's queue
          if new_Plane is not None:
