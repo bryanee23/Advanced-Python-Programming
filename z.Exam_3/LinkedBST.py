31b182184e00dda5efba515824a6a3551fbd5870
Bryan Evangelista <bryanevangelista@Bryans-MBP.socal.rr.com>
2020-12-12 15:52:54 -0800

    finished prompt 4

--- z.Exam_3/LinkedBST.py
+++ z.Exam_3/LinkedBST.py
@@ -4,115 +4,162 @@ from BinaryNode import BinaryNode
 
 
 class LinkedBST(AbstractCollection):
+    """ A linked version of a binary search tree """
 
-	""" A linked version of a binary search tree """
-	def __init__(self):
-		self._root = None
-		AbstractCollection.__init__(self)
+    def __init__(self):
+        self._root = None
+        AbstractCollection.__init__(self)
 
-# breadth:
-#      Create a queue
-#      Add root to the queue
-#      Loop until queue is empty:
-#           print Node at front of queue
-#           pop Node from queue
-#           add Node’s left and right children to queue
-	def bfs_akabreakfast(self):
+    # edited code to answer prompt 1
+    def find(self, item):
+        visited_nodes = []
+        none_node_visit = []
+        """ Returns 'item' if it's found, and 'None' otherwise """
+        def recurse(node):
+            # Base case 1
+            if node is None:
 
+                # if reaches here, not in the tree
+                print(
+                    f"\nSearch complete, not found. {len(visited_nodes)} Node visited.")
+                none_node_visit.append(node)
+                return None
 
-	# edited code to answer promt 1
-	def find(self, item):
-		visited_nodes = []
-		none_node_visit = []
-		""" Returns 'item' if it's found, and 'None' otherwise """
-		def recurse(node):
-			# Base case 1
-			if node is None:
+            # Base case 2
+            elif node.data == item:
 
-				# if reaches here, not in the tree
-				print(f"\nSearch complete, not found. {len(visited_nodes)} Node visited.")
-				none_node_visit.append(node)
-				return None
+                # handles if item is the root, and if in the tree
+                if len(visited_nodes) == 0:
+                    print(
+                        f"\nSearch complete, {node.data} found. 0 Node visited")
+                else:
+                    print(
+                        f"\nSearch complete, {node.data} found. {len(visited_nodes)} Node visited.")
 
-			# Base case 2
-			elif node.data == item:
+                return node.data
 
-				# handles if item is the root, and if in the tree
-				if len(visited_nodes) == 0:
-					print(f"\nSearch complete, {node.data} found. 0 Node visited")
-				else:
-					print(f"\nSearch complete, {node.data} found. {len(visited_nodes)} Node visited.")
+            elif item < node.data:
+                visited_nodes.append(node.data)
+                return recurse(node.left)
+            else:
+                visited_nodes.append(node.data)
+                return recurse(node.right)
 
-				return node.data
+        return recurse(self._root)
 
-			elif item < node.data:
-				visited_nodes.append(node.data)
-				return recurse(node.left)
-			else:
-				visited_nodes.append(node.data)
-				return recurse(node.right)
+    def height(self):
+        """ Returns the current height of the tree """
+        def recurse(node):
+            # Base case
+            if node is None:
+                return 0
+            return 1 + max(recurse(node.left), recurse(node.right))
+        return recurse(self._root)
 
-		return recurse(self._root)
+    # code to answer prompt 2
+    def height_from_node(self):
+        """
+        calls BinaryNode's height method. similar to linkedBST's height method but further down the pipline
+        """
+        return BinaryNode.node_height(self._root)
 
-	def height(self):
-		""" Returns the current height of the tree """
-		def recurse(node):
-			# Base case
-			if node is None:
-				return 0
-			return 1 + max(recurse(node.left), recurse(node.right))
-		return recurse(self._root)
+		# code to answer prompt 3
+    def bfs_akabreakfast(self):
+        """
+        q = []
+        q.add(self.root)
+        while q.isempty is not true:
+                popped = pop@
+                print(pop.q.data)
+                q.add(popped.left)
+                q.add(popped.pop.right)
+        """
+        q_silent_ueue = LinkedQueue()
+        q_silent_ueue.add(self._root)
+        visited = []
 
-	# code to answer promt 2
-	def height_from_node(self):
-		"""
-		calls BinaryNode's height method. similar to linkedBST's height method but further down the pipline
-		"""
-		return BinaryNode.node_height(self._root)
+        while q_silent_ueue.is_empty is not True:
+            popped = q_silent_ueue.pop()
 
-	def inorder(self):
+            if popped is not None:
+                visited.append(popped.data)
+                print(popped.data, end=" ")
 
-		def recurse(node):
-			if node is None:
-				return
-			recurse(node.left)
-			print(node.data, end=" ")
-			recurse(node.right)
+                if popped.left is not None:
+                    q_silent_ueue.add(popped.left)
+                if popped.right is not None:
+                    q_silent_ueue.add(popped.right)
 
-		recurse(self._root)
+            if q_silent_ueue.is_empty():
+                break
 
-	# created to provide visual on tree insertion order
-	def preorder(self):
+        # for testing returns array of visited values
+        return visited
 
-		def recurse(node):
-			if node is None:
-				return
-			print(node.data, end=" ")
-			recurse(node.left)
-			recurse(node.right)
+		# code to answer prompt 4
+    def is_balanced(self):
+        # handle single node
+        if self._root is not None:
+            if self._root.left is None and self._root.right is None:
+                return True
 
-		recurse(self._root)
+        q = []
+        q.append(self._root)
+        while len(q) > 0:
+            popped = q.pop(0)
 
-	def add(self, item):
-		""" Adds 'item' to the binary search tree """
+            if popped is not None:
+                # handles 3 conditions
+                if popped.left is None and popped.right is not None:
+                    return False
+                elif popped.right is None and popped.left is not None:
+                    return False
 
-		def recurse(node):
-			""" Inner Function to find a new item's location in the tree """
-			if item < node.data:
-				if node.left is None:
-					node.left = BinaryNode(item)
-				else:
-					recurse(node.left)
+                else: # left and right node is not none
+                    if popped.left is not None:
+                        q.append(popped.left)
 
-			elif item >= node.data:
-				if node.right is None:
-					node.right = BinaryNode(item)
-				else:
-					recurse(node.right)
+                    if popped.right is not None:
+                        q.append(popped.right)
+        return True
 
-		if self.is_empty():
-			self._root = BinaryNode(item)
-		else:
-			recurse(self._root)
+    def preorder(self):
+        def recurse(node):
+            if node is None:
+                return
+            print(node.data, end=" ")
+            recurse(node.left)
+            recurse(node.right)
+        recurse(self._root)
 
-		self._size += 1
+    def inorder(self):
+        def recurse(node):
+            if node is None:
+                return
+            recurse(node.left)
+            print(node.data, end=" ")
+            recurse(node.right)
+        recurse(self._root)
+
+    def add(self, item):
+        """ Adds 'item' to the binary search tree """
+        def recurse(node):
+            """ Inner Function to find a new item's location in the tree """
+            if item < node.data:
+                if node.left is None:
+                    node.left = BinaryNode(item)
+                else:
+                    recurse(node.left)
+
+            elif item >= node.data:
+                if node.right is None:
+                    node.right = BinaryNode(item)
+                else:
+                    recurse(node.right)
+
+        if self.is_empty():
+            self._root = BinaryNode(item)
+        else:
+            recurse(self._root)
+
+        self._size += 1
--- z.Exam_3/Main.py
+++ z.Exam_3/Main.py
@@ -1,100 +1,202 @@
-import unittest, random
+import unittest
+import random
 from LinkedQueue import LinkedQueue
 from LinkedBST import LinkedBST
 from BinaryNode import BinaryNode
 
 
+# replace Test_extraCredit() w/ Test_extraCredit(unittest.TestCase) to run
+class Test_extraCredit(unittest.TestCase):
+    """ unittest.TestCase super class removed to prevent output congestion in terminal"""
+    def test_bonus(self):
+        tree = LinkedBST()
+        tree.add(4)
+        tree.add(6)
+
+
+
+# replace Test_4() w/ Test_4(unittest.TestCase) to run
+class Test_4():
+    """ unittest.TestCase super class removed to prevent output congestion in terminal"""
+
+    def test_4a(self):
+        tree = LinkedBST()
+        tree.add(4)
+        self.assertEqual(tree.is_balanced(), True)
+
+    def test_4b(self):
+        tree = LinkedBST()
+        tree.add(4)
+        tree.add(3)
+        self.assertEqual(tree.is_balanced(), False)
+        tree.add(5)
+        check = tree.is_balanced()
+        self.assertEqual(tree.is_balanced(), True)
+                # 4
+            # 3      5
+
+    def test_4c(self):
+        tree = LinkedBST()
+        tree.add(5)
+        tree.add(3)
+        tree.add(1)
+        tree.add(4)
+        tree.add(6)
+        # tree.preorder()
+        self.assertEqual(tree.is_balanced(), True)
+                # 5
+            # 3      6
+        # 1     4
+
+
+
 # replace Test_3() w/ Test_3(unittest.TestCase) to run
-class Test_3(unittest.TestCase):
-  """ unittest.TestCase super class removed to prevent output congestion in terminal"""
+class Test_3():
+    """ unittest.TestCase super class removed to prevent output congestion in terminal"""
 
-  def test_3a(self):
-    tree = LinkedBST()
-    tree.add(4)
-    tree.add(2)
-    tree.add(5)
-    tree.bfs_akabreakfast()
+    def test_3a(self):
+        """ local test to verify method """
+        tree = LinkedBST()
+        tree.add(4)
+        tree.add(2)
+        tree.add(5)
+        self.assertEqual(tree.bfs_akabreakfast(), [4, 2, 5])
+        print('\n')
 
+    def test_3b(self):
+        """ generate random inserts and see what happens """
+        tree = LinkedBST()
+        # tree.add(random.randrange(3,6))
+        # tree.add(random.randrange(3,5))
+        # tree.add(random.randrange(1,3))
+        # tree.add(random.randrange(4,7))
+        # tree.add(random.randrange(0,9))
+        # tree.add(random.randrange(3,6))
+        # tree.add(random.randrange(3,5))
+        # tree.add(random.randrange(1,3))
+        # tree.add(random.randrange(4,7))
+        # tree.add(random.randrange(0,9))
+        # tree.add(random.randrange(0,9))
+        # tree.preorder()
+        tree.add(4)
+        tree.add(1)
+        tree.add(4)
+        tree.add(4)
+        tree.add(5)
+        tree.add(8)
+        self.assertEqual(tree.bfs_akabreakfast(), [4, 1, 4, 4, 5, 8])
+        print('\n')
 
+        #   4
+        # 1   4
+        #       4
+        #         5
+        #           8
+
+    def test_3c(self):
+        """ generate random inserts and see what happens """
+        tree = LinkedBST()
+        # tree.add(random.randrange(3,6))
+        # tree.add(random.randrange(3,5))
+        # tree.add(random.randrange(1,3))
+        # tree.add(random.randrange(4,7))
+        # tree.add(random.randrange(0,9))
+        # tree.add(random.randrange(0,9))
+        # tree.preorder()
+        tree.add(3)
+        tree.add(1)
+        tree.add(1)
+        tree.add(1)
+        tree.add(3)
+        tree.add(6)
+        tree.add(7)
+        self.assertEqual(tree.bfs_akabreakfast(), [3, 1, 3, 1, 6, 1, 7])
+        print('\n')
+
+        #   3
+        # 1   3
+        #   1   6
+        #     1   7
+        #
 
 
 # replace Test_2() w/ Test_2(unittest.TestCase) to run
-class Test_2(unittest.TestCase):
-  """ unittest.TestCase super class removed to prevent output congestion in terminal"""
+class Test_2():
+    """ unittest.TestCase super class removed to prevent output congestion in terminal"""
 
-  def test_2a(self):
-    """ local tests to node height method """
-    tree = LinkedBST()
-    tree.add(4)
-    tree.add(3)
-    self.assertEqual(tree.height_from_node(), 2)
-    tree.add(2)
-    self.assertEqual(tree.height_from_node(), 3)
+    def test_2a(self):
+        """ local tests to node height method """
+        tree = LinkedBST()
+        tree.add(4)
+        tree.add(3)
+        self.assertEqual(tree.height_from_node(), 2)
+        tree.add(2)
+        self.assertEqual(tree.height_from_node(), 3)
 
-  def test_2b(self):
-    """ randomized test for verification """
-    tree = LinkedBST()
-    # tree.add(random.randrange(3,6))
-    # tree.add(random.randrange(3,5))
-    # tree.add(random.randrange(1,3))
-    # tree.add(random.randrange(4,7))
-    # tree.add(random.randrange(0,9))
-    tree.add(3)
-    tree.add(2)
-    tree.add(3)
-    tree.add(5)
-    tree.add(6)
-    # tree.preorder()
-    self.assertEqual(tree.height_from_node(), 4)
+    def test_2b(self):
+        """ randomized test for verification """
+        tree = LinkedBST()
+        # tree.add(random.randrange(3,6))
+        # tree.add(random.randrange(3,5))
+        # tree.add(random.randrange(1,3))
+        # tree.add(random.randrange(4,7))
+        # tree.add(random.randrange(0,9))
+        tree.add(3)
+        tree.add(2)
+        tree.add(3)
+        tree.add(5)
+        tree.add(6)
+        # tree.preorder()
+        self.assertEqual(tree.height_from_node(), 4)
 
 
 # replace Test_1() w/ Test_1(unittest.TestCase) to run
 class Test_1():
-  """ unittest.TestCase super class removed to prevent output congestion in terminal"""
+    """ unittest.TestCase super class removed to prevent output congestion in terminal"""
 
-  def test_1a(self):
-    """ test connection """
-    tree = LinkedBST()
-    self.assertEqual(tree.is_empty(), True)
-    tree.add(random.randrange(5,6))
-    tree.add(random.randrange(3,5))
-    tree.add(random.randrange(4,7))
-    tree.inorder()
-    self.assertEqual(len(tree), 3)
+    def test_1a(self):
+        """ test connection """
+        tree = LinkedBST()
+        self.assertEqual(tree.is_empty(), True)
+        tree.add(random.randrange(5, 6))
+        tree.add(random.randrange(3, 5))
+        tree.add(random.randrange(4, 7))
+        # tree.inorder()
+        self.assertEqual(len(tree), 3)
 
-  def test_1b(self):
-    """ local test """
-    tree = LinkedBST()
-    tree.add(4)
-    tree.add(3)
-    tree.add(4)
-    tree.add(2)
-    tree.add(5)
-    print("\n\n Question 1 ")
-    tree.preorder()
-    tree.find(5) # prints 1 node visited
-    tree.find(2) # prints 2 node visited
-    tree.find(0) # prints 2 node visited
+    def test_1b(self):
+        """ local test """
+        tree = LinkedBST()
+        tree.add(4)
+        tree.add(3)
+        tree.add(4)
+        tree.add(2)
+        tree.add(5)
+        print("\n\n Question 1 ")
+        # tree.preorder()
+        tree.find(5)  # prints 1 node visited
+        tree.find(2)  # prints 2 node visited
+        tree.find(0)  # prints 2 node visited
 
-  def test_1c(self):
-    """ ran random numbers then replaced to verify """
-    tree = LinkedBST()
-    # tree.add(random.randrange(3,6))
-    # tree.add(random.randrange(3,5))
-    # tree.add(random.randrange(1,3))
-    # tree.add(random.randrange(4,7))
-    # tree.add(random.randrange(0,9))
-    tree.add(5)
-    tree.add(3)
-    tree.add(1)
-    tree.add(1)
-    tree.add(6)
-    # tree.preorder() # test random generated tree
-    tree.find(5) # prints 0 node visited
-    tree.find(1) # prints 2 node visited
-    tree.find(0) # prints 3 node visited
-    tree.find(6) # prints 1 node visited
+    def test_1c(self):
+        """ ran random numbers then replaced to verify """
+        tree = LinkedBST()
+        # tree.add(random.randrange(3,6))
+        # tree.add(random.randrange(3,5))
+        # tree.add(random.randrange(1,3))
+        # tree.add(random.randrange(4,7))
+        # tree.add(random.randrange(0,9))
+        tree.add(5)
+        tree.add(3)
+        tree.add(1)
+        tree.add(1)
+        tree.add(6)
+        # tree.preorder() # test random generated tree
+        tree.find(5)  # prints 0 node visited
+        tree.find(1)  # prints 2 node visited
+        tree.find(0)  # prints 3 node visited
+        tree.find(6)  # prints 1 node visited
 
 
 if __name__ == "__main__":
-  unittest.main()
+    unittest.main()
